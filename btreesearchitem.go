db601cde40d36661e9d3daca5119fc7cca48bdae
Nurudeen <nurudeenbolaman@gmail.com>
2025-11-11 14:40:43 +0100

    piscine

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -3,16 +3,16 @@ package piscine
 // BTreeSearchItem searches for a node with Data equal to elem in the BST rooted
 // at root and returns the node pointer if found, otherwise nil.
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-    cur := root
-    for cur != nil {
-        if elem == cur.Data {
-            return cur
-        }
-        if elem < cur.Data {
-            cur = cur.Left
-        } else {
-            cur = cur.Right
-        }
-    }
-    return nil
+	cur := root
+	for cur != nil {
+		if elem == cur.Data {
+			return cur
+		}
+		if elem < cur.Data {
+			cur = cur.Left
+		} else {
+			cur = cur.Right
+		}
+	}
+	return nil
 }
