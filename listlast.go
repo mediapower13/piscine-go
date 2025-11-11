ef92a9a4692803ef6f9f914e66b2b6ac5d291c50
Nurudeen <nurudeenbolaman@gmail.com>
2025-11-11 10:43:36 +0100

    piscine

--- listlast.go
+++ listlast.go
@@ -3,12 +3,12 @@ package piscine
 // ListLast returns the Data of the last element of the list l.
 // If l is nil or empty, it returns nil.
 func ListLast(l *List) interface{} {
-    if l == nil || l.Head == nil {
-        return nil
-    }
-    cur := l.Head
-    for cur.Next != nil {
-        cur = cur.Next
-    }
-    return cur.Data
+	if l == nil || l.Head == nil {
+		return nil
+	}
+	cur := l.Head
+	for cur.Next != nil {
+		cur = cur.Next
+	}
+	return cur.Data
 }
